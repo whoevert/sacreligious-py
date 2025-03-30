d9b41c8ebe208aa9adaac06353bfe4629a4fe812
whoever <stekirand@gmail.com>
2025-03-30 12:27:30 +0700

    idk wow

--- maingame.py
+++ maingame.py
@@ -6,33 +6,32 @@ pygame.init()
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-tile_s = 25
+tile_s = 50
+tank_s = tile_s * 2
 
-vis_world_w = 25
-vis_world_h = 25
+vis_world_w = 16
+vis_world_h = 9
 
 scr_w = tile_s * vis_world_w
 scr_h = tile_s * vis_world_h
 screen = pygame.display.set_mode((scr_w, scr_h))
 
 gras = pygame.image.load('tiles/grass.jpg')
-gron = pygame.image.load('tiles/ground.jpg')
 ston = pygame.image.load('tiles/stone.jpg')
 watr = pygame.image.load('tiles/water.jpg')
-lava = pygame.image.load('tiles/lava.jpg')
 
 gras = pygame.transform.scale(gras, (tile_s, tile_s))
-gron = pygame.transform.scale(gron, (tile_s, tile_s))
 ston = pygame.transform.scale(ston, (tile_s, tile_s))
 watr = pygame.transform.scale(watr, (tile_s, tile_s))
-lava = pygame.transform.scale(lava, (tile_s, tile_s))
+
+default_tile = gras
 
 
 class Direction(Enum):
-    Up = 0
-    Down = 180
-    Left = 90
-    Right = 270
+    Up = 90
+    Down = 270
+    Left = 180
+    Right = 0
 
 
 world_h = 25
@@ -50,8 +49,6 @@ y_shift = 0
 
 name_to_tile = {
     'gras': gras,
-    'gron': gron,
-    'lava': lava,
     'ston': ston,
     'watr': watr
 }
@@ -60,8 +57,8 @@ name_to_tile = {
 class Tank:
     def __init__(self, tile, x, y):
         self.tile = pygame.image.load(f'tiles/{tile}')
-        self.w = tile_s
-        self.h = tile_s
+        self.w = tank_s
+        self.h = tank_s
         self.s = (self.w, self.h)
 
         self.tile = pygame.transform.scale(self.tile, self.s)
@@ -70,17 +67,71 @@ class Tank:
         self.y = tile_s * y + tile_s // 2
 
         self.dir = Direction.Right
-        self.speed = 2
+        self.speed = tile_s / 12
         self.on_move = False
 
+    def update_rect(self):
+        pass
+
     def render(self, screen):
-        self.rect.centerx = self.x
-        self.rect.centery = self.y
+        self.update_rect()
         tile = pygame.transform.rotate(
             self.tile,
             self.dir.value
         )
         screen.blit(tile, self.rect)
+
+    def change_dir(self, direction: Direction):
+        self.dir = dir
+
+class PlayerTank(Tank):
+
+    def update_rect(self):
+        self.rect.centerx = round(self.x)
+        self.rect.centery = round(self.y)
+
+    def move(self):
+        global world
+        global x_shift, y_shift
+
+        weight_horld = world_h * tile_s
+        width_world = world_w * tile_s
+        if not self.on_move:
+            return
+
+        if self.dir == Direction.Up:
+            if self.y <= scr_h / 2 and y_shift - self.speed >= 0:
+                y_shift -= self.speed
+            else:
+                self.y -= self.speed
+
+        if self.dir == Direction.Down:
+            if self.y >= scr_h / 2 and \
+                y_shift + self.speed <= weight_horld - scr_h:
+
+                y_shift += self.speed
+            else:
+                self.y += self.speed
+
+        if self.dir == Direction.Left:
+            if self.x <= scr_w / 2 and x_shift - self.speed >= 0:
+                x_shift -= self.speed
+            else:
+                self.x -= self.speed
+
+        if self.dir == Direction.Right:
+            if self.x >= scr_w / 2 and \
+                x_shift + self.speed <= width_world - scr_w:
+
+                x_shift += self.speed
+            else:
+                self.x += self.speed
+
+class EnemyMank(Tank):
+
+    def update_rect(self):
+        self.rect.centerx = round(self.x - x_shift)
+        self.rect.centery = round(self.y - y_shift)
 
     def move(self):
         if not self.on_move:
@@ -131,8 +182,8 @@ def load_world():
     file.close()
 
 
-tank = Tank('tank.png', 3, 3)
-mank = Tank('mank.png', 21, 21)
+tank = PlayerTank('tank.png', 3, 3)
+mank = EnemyMank('mank.png', 21, 21)
 
 load_world()
 
@@ -146,13 +197,13 @@ while run:
 
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_UP:
-            tank.dir = Direction.Up
+            tank.change_dir(Direction.Up)
         if event.key == pygame.K_DOWN:
-            tank.dir = Direction.Down
+            tank.change_dir(Direction.Down)
         if event.key == pygame.K_LEFT:
-            tank.dir = Direction.Left
+            tank.change_dir(Direction.Left)
         if event.key == pygame.K_RIGHT:
-            tank.dir = Direction.Right
+            tank.change_dir(Direction.Right)
 
     if event.type == pygame.KEYDOWN and event.key in arrows:
         tank.on_move = True
@@ -160,18 +211,16 @@ while run:
     if event.type == pygame.KEYUP and event.key in arrows:
         tank.on_move = False
 
-    for i in range(vis_world_h):
-        for j in range(vis_world_w):
-            x = j * tile_s
-            y = i * tile_s
+    for i in range(world_h):
+        for j in range(world_w):
+            x = round(j * tile_s - x_shift)
+            y = round(i * tile_s - y_shift)
 
-            cell = world[i + y_shift][j + x_shift]
+            screen.blit(default_tile, (x, y))
+
+            cell = world[i][j]
             if cell is not None:
                 screen.blit(cell, (x, y))
-            else:
-                cell_w = tile_s
-                cell_h = tile_s
-                pygame.draw.rect(screen, white, (x, y, cell_w, cell_h), 1)
 
     tank.move()
     tank.render(screen)
@@ -179,7 +228,7 @@ while run:
     mank.move()
     mank.render(screen)
 
-    pygame.time.delay(50)
+    pygame.time.delay(25)
     pygame.display.update()
 
 pygame.quit()
